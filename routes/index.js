34ab7d4bf1c91e5278d33a93d6eea30fec0e1f01
Sacha Nacar <snacar@cisco.com>
2017-01-26 22:01:37 -0800

    added welcome logic;

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,32 @@
 var express = require('express');
 var router = express.Router();
-var Spark = require('csco-spark');
-var spark = Spark({
-  uri: 'https://api.ciscospark.com/v1',
-  token: ''
-});
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  spark.sendMessage({
-    roomId:'',
-    files: 'https://github.com/miradaa/icon/blob/master/message11.png?raw=true'
-  }).then((res) => {
-    /*Store the res data?*/
-  });
+const ciscospark = require(`ciscospark`);
+router.post('/bot', function(req, res, next) {
+  if (req.body.data.personEmail != 'sara@sparkbot.io'){
+    var data = req.body.data;
+    getMsg(data).then(function(m) {
+      text = getMsgText(m);
+      if(text.includes('here')){
+        welcome(m);
+      }
+    });
+  }
 });
 
+function welcome(m){
+  ciscospark.people.get(m.personId).then(function(person) {
+    return ciscospark.messages.create({
+      text: 'Hey '+person.displayName.split(' ')[0]+'! How are you?',
+      roomId: m.roomId
+    });
+  });
+}
+function getMsg(message){
+  return ciscospark.messages.get(message.id);
+}
+
+function getMsgText(message){
+  var text = message.text.split('Sara')[1];
+  return text;
+}
 module.exports = router;
